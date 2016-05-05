correct lookahead gradient

tensorflow/contrib/lookahead/lookahead_grad_op_test.py
@@ -10,19 +10,19 @@ class LookaheadGradTest(tf.test.TestCase):
   def test(self):
     with self.test_session():
       x1 = [[[1.0,2.0,3.0],[4.0,5.0,6.0],[7.0,8.0,9.0]], [[1.0,2.0,3.0],[4.0,5.0,6.0],[7.0,8.0,9.0]]]
-      x2 = [[[1.0,2.0,4.0],[4.0,8.0,16.0]], [[1.0,2.0,4.0],[4.0,8.0,16.0]]]
+      x2 = [[1.0,2.0,4.0],[4.0,8.0,16.0]]
       x3 = [[[1.0,2.0,3.0],[4.0,5.0,6.0],[7.0,8.0,9.0]], [[1.0,2.0,3.0],[4.0,5.0,6.0],[7.0,8.0,9.0]]]
       result = tf.contrib.lookahead.lookaheadgradinputcpu(x1,x2,x3)
       self.assertAllEqual(result.eval(), [[[1.0,2.25,3.5],[2.0,3.0,3.625],[1.75,2.4375,2.75]], [[1.0,2.25,3.5],[2.0,3.0,3.625],[1.75,2.4375,2.75]]])
       result_2 = tf.contrib.lookahead.lookaheadgradinputgpu(x1,x2,x3)
       self.assertAllEqual(result_2.eval(), [[[1.0,2.25,3.5],[2.0,3.0,3.625],[1.75,2.4375,2.75]], [[1.0,2.25,3.5],[2.0,3.0,3.625],[1.75,2.4375,2.75]]])
       y1 = [[[1.0,2.0,4.0,8.0],[4.0,8.0,16.0,32.0],[8.0,16.0,32.0,64.0]], [[1.0,2.0,4.0,8.0],[4.0,8.0,16.0,32.0],[8.0,16.0,32.0,64.0]]]
-      y2 = [[[1.0,2.0,4.0],[4.0,8.0,16.0]], [[1.0,2.0,4.0],[4.0,8.0,16.0]]]
+      y2 = [[1.0,2.0,4.0],[4.0,8.0,16.0]]
       y3 = [[[1.0,2.0,4.0,8.0],[4.0,8.0,16.0,32.0],[8.0,16.0,32.0,64.0]], [[1.0,2.0,4.0,8.0],[4.0,8.0,16.0,32.0],[8.0,16.0,32.0,64.0]]]
       result_filter = tf.contrib.lookahead.lookaheadgradfiltercpu(y1,y2,y3)
-      self.assertAllEqual(result_filter.eval(), [[[4.0,4.0,4.0],[1.5,1.5,1.5]], [[4.0,4.0,4.0],[1.5,1.5,1.5]]])
+      self.assertAllEqual(result_filter.eval(), [[8.0,8.0,8.0],[3.0,3.0,3.0]])
       result_filter_2 = tf.contrib.lookahead.lookaheadgradfiltergpu(y1,y2,y3)
-      self.assertAllEqual(result_filter_2.eval(), [[[4.0,4.0,4.0],[1.5,1.5,1.5]], [[4.0,4.0,4.0],[1.5,1.5,1.5]]])
+      self.assertAllEqual(result_filter_2.eval(), [[8.0,8.0,8.0],[3.0,3.0,3.0]])
       print(result.eval())
       print(result_2.eval())
       print(result_filter.eval())

tensorflow/contrib/lookahead/lookahead_op_test.py
@@ -11,7 +11,7 @@ class LookaheadTest(tf.test.TestCase):
   def test(self):
     with self.test_session():
       x1 = [[[1.0,2.0,3.0],[4.0,5.0,6.0],[7.0,8.0,9.0]], [[1.0,2.0,3.0],[4.0,5.0,6.0],[7.0,8.0,9.0]]]
-      x2 = [[[1.0,2.0,3.0],[4.0,5.0,6.0]], [[1.0,2.0,3.0],[4.0,5.0,6.0]]]
+      x2 = [[1.0,2.0,3.0],[4.0,5.0,6.0]]
       result = tf.contrib.lookahead.lookaheadcpu(x1,x2)
       self.assertAllEqual(result.eval(), [[[9.0,14.0,3.0],[33.0,40.0,12.0],[69.0,78.0,27.0]], [[9.0,14.0,3.0],[33.0,40.0,12.0],[69.0,78.0,27.0]]])
       result_2 = tf.contrib.lookahead.lookaheadgpu(x1,x2)

tensorflow/contrib/lookahead/lookahead_ops.py
@@ -71,6 +71,8 @@ def _Lookaheadcpu_grad(op, grad):
   Returns:
     the gradients w.r.t. the input and the filter
   """
+  print(op.inputs[0].get_shape())
+  print(op.inputs[1].get_shape())
   return [tf.contrib.lookahead.lookaheadgradinputcpu(
               op.inputs[0],op.inputs[1],grad),
           tf.contrib.lookahead.lookaheadgradfiltercpu(
@@ -109,12 +111,12 @@ def _Lookaheadgradinputgpu(op):
 
 @ops.RegisterShape("Lookaheadgradfiltercpu")
 def _Lookaheadgradfiltercpu(op):
-  inputs_shape = op.inputs[1].get_shape().with_rank(3)
+  inputs_shape = op.inputs[1].get_shape().with_rank(2)
   return [inputs_shape]
 
 @ops.RegisterShape("Lookaheadgradfiltergpu")
 def _Lookaheadgradfiltergpu(op):
-  inputs_shape = op.inputs[1].get_shape().with_rank(3)
+  inputs_shape = op.inputs[1].get_shape().with_rank(2)
   return [inputs_shape]
 
 @ops.RegisterGradient("Lookaheadgradinputcpu")